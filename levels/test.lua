52ad383a7425952a8e1582e9292e7c38cb8e64f8
Stephen Roantree <stroantree@gmail.com>
2012-08-27 23:48:14 +0100

    Changed the demo slightly.

--- levels/test.lua
+++ levels/test.lua
@@ -4,11 +4,12 @@ local tileSheet = SpriteSheet:new("images/tiles.png", 64, 64, 32, 48)
 local tileHeights = {0, 32, 16}
 local tileSprites = TileSprites:new(tileSheet, tileHeights)
 
-local map = {{2, 3, 1, 1, 1}
-		    ,{1, 1, 3, 1, 1}
-		    ,{1, 1, 3, 1, 1}
-		    ,{3, 1, 1, 1, 3}
-		    ,{1, 1, 1, 1, 3}}
+local map = {{2, 3, 1, 1, 1, 1}
+		    ,{1, 1, 3, 1, 1, 1}
+		    ,{1, 1, 3, 1, 1, 1}
+		    ,{3, 1, 1, 1, 3, 1}
+		    ,{1, 1, 1, 1, 3, 1}
+		    ,{1, 1, 1, 1, 1, 1}}
 
 local objectSheet = SpriteSheet:new("images/big_object.png", 64, 128, 32, 112)
 local bigObject = TileObject:new(objectSheet, 1, 96)
@@ -16,6 +17,9 @@ local bigObject = TileObject:new(objectSheet, 1, 96)
 local tileMap = TileMap:new(tileSprites, map)
 tileMap:addObject(bigObject, 1, 1)
 tileMap:addObject(bigObject, 2, 1)
+tileMap:addObject(bigObject, 3, 1)
+tileMap:addObject(bigObject, 2, 2)
+tileMap:addObject(bigObject, 3, 2)
 tileMap:addObject(bigObject, 1, 5)
 tileMap:process()
 
