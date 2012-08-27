142f45024443a5d2c905db201013eba35d31a9b3
Stephen Roantree <stroantree@gmail.com>
2012-08-27 16:02:06 +0100

    Fix indentation

--- lib/spritesheet.lua
+++ lib/spritesheet.lua
@@ -1,30 +1,30 @@
 SpriteSheet = {}
 SpriteSheet.__index = SpriteSheet
 function SpriteSheet:new(filename, tileWidth, tileHeight)
-	local img = love.graphics.newImage(filename)
-	local imgWidth = img:getWidth()
-	local imgHeight = img:getHeight()
+    local img = love.graphics.newImage(filename)
+    local imgWidth = img:getWidth()
+    local imgHeight = img:getHeight()
 
-	-- Generate quads for the tiles
-	local cols = math.floor(imgWidth / tileWidth)
+    -- Generate quads for the tiles
+    local cols = math.floor(imgWidth / tileWidth)
     local rows = math.floor(imgHeight / tileHeight)
     local quads = {}
     local i = 0
 
     for y=0,(rows-1) do
-    	local ycoord = tileHeight * y
-    	for x=0,(cols-1) do
-    		local xcoord = tileWidth * x
-    		quads[i] = love.graphics.newQuad(xcoord, ycoord, tileWidth, tileHeight, imgWidth, imgHeight)
-    		i = i + 1
-    	end
+        local ycoord = tileHeight * y
+        for x=0,(cols-1) do
+            local xcoord = tileWidth * x
+            quads[i] = love.graphics.newQuad(xcoord, ycoord, tileWidth, tileHeight, imgWidth, imgHeight)
+            i = i + 1
+        end
     end
 
-	local o = { img = img, quads = quads, tileWidth = tileWidth, tileHeight = tileHeight }
-	setmetatable(o, self)
-	return o
+    local o = { img = img, quads = quads, tileWidth = tileWidth, tileHeight = tileHeight }
+    setmetatable(o, self)
+    return o
 end
 
 function SpriteSheet:draw(index, x, y)
-	love.graphics.drawq(self.img, self.quads[index], x, y)
+    love.graphics.drawq(self.img, self.quads[index], x, y)
 end
