a5c8ca1a10aa2d4938a2a268cdf5e1b052c13449
Stephen Roantree <stroantree@gmail.com>
2012-08-27 15:56:38 +0100

    Sprite sheet loading / rendering.

--- main.lua
+++ main.lua
@@ -1,5 +1,8 @@
+require 'lib/spritesheet'
+
 function love.load()
-	x = 0
+	tileSheet = SpriteSheet:new("images/tiles.png", 64, 64)
+	love.graphics.setBackgroundColor(0,0,0)
 end
 
 function love.update(dt)
@@ -7,5 +10,9 @@ function love.update(dt)
 end
 
 function love.draw()
-	love.graphics.print("Hello World", x, 300)
+	love.graphics.clear()
+	tileSheet:draw(1, 128, 48)
+	tileSheet:draw(1, 32, 32)
+	tileSheet:draw(2, 64, 48)
+	tileSheet:draw(0, 96, 64)
 end
